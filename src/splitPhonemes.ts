fccf6e7f41bcffe6fcd25024e091fc377b23db0f
Fernando Andrés García Hernández <f.garcia.ext@docline.com>
2025-05-30 18:24:57 +0100

    green: considers diagraph

--- src/splitPhonemes.ts
+++ src/splitPhonemes.ts
@@ -1,7 +1,16 @@
+const diagraphs = ["ll"]
+
 export const splitPhonemes = (word: string) => {
   let phonemes = []
-  for (const i of word) {
-    phonemes.push(i)
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === "l" && word[i + 1] === "l") {
+      phonemes.push(`${word[i]}${word[i + 1]}`)
+      i++
+      continue
+    }
+
+    phonemes.push(word[i])
   }
   return phonemes
 }
